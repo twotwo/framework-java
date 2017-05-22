c7d79602bc5e66ceb040598eb826a3a36274b4b4
twotwo <twotwo.li@gmail.com>
2017-05-22 18:14:24 +0800

    1. 优化Common CLI处理逻辑 2. 添加log4j2日志 3. 添加版本生成和显示逻辑

--- mvn_uber_jar/src/main/java/com/li3huo/maven/App.java
+++ mvn_uber_jar/src/main/java/com/li3huo/maven/App.java
@@ -1,6 +1,8 @@
 package com.li3huo.maven;
 
+import java.io.InputStream;
 import java.util.Date;
+import java.util.Properties;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -9,7 +11,10 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.li3huo.service.TimeServer;
 
@@ -17,76 +22,98 @@ import com.li3huo.service.TimeServer;
  * 
  * @author liyan
  * 
- *         use commoms-cli for Commandline Parser
+ *         use
+ *         <a href="http://commons.apache.org/proper/commons-cli/usage.html">
+ *         commoms-cli</a> for Commandline Parser
  */
 public class App {
 
-	private static Options options;
+	static final Logger logger = LogManager.getLogger(App.class.getName());
+	/** print for help */
+	static Options options;
 
-	private static synchronized Options getOptions() {
-		if (null == options) {
+	/**
+	 * 
+	 * @return options
+	 */
+	private static Options initOptions() {
 
-			Option help = new Option("help", "print this message");
-			Option version = new Option("version", "print the version information and exit");
-			//https://tools.ietf.org/html/rfc868
-			Option server = new Option("server", "start a time server.");
+		Options options = new Options();
+		options.addOption("help", "print this message");
+		options.addOption("version", "print the version information and exit");
+		// https://tools.ietf.org/html/rfc868
+		// -s or --server [port]
+		options.addOption(Option.builder("s").longOpt("server").hasArg().argName("port").optionalArg(true)
+				.desc("start sdk-agent server on [port]. default is 8000").build());
 
-			options = new Options();
-			options.addOption(help);
-			options.addOption(server);
-			options.addOption(version);
-			options.addOption("t", false, "display current time");
-		}
+		options.addOption("t", false, "display current time");
 
 		return options;
+
 	}
 
-	private static void parseCommandLine(String[] args) {
-		Options options = getOptions();
+	private static CommandLine parseCommandLine(String[] args) {
+		options = initOptions();
 
 		CommandLineParser parser = new DefaultParser();
 
 		try {
 			CommandLine cmd = parser.parse(options, args);
-
-			// System.out.println("options: "+cmd.getOptions().length);
-			// System.out.println("args: "+cmd.getArgList().size());
-
-			if (0 == cmd.getOptions().length + cmd.getArgList().size() || cmd.hasOption("help")) {
-				// automatically generate the help statement
-				HelpFormatter formatter = new HelpFormatter();
-				formatter.printHelp("help", options);
-				return;
-			}
-			
-			if (cmd.hasOption("server")) {
-				new TimeServer().run();
-			}
-			
-			if (cmd.hasOption("version")) {
-				//@TODO 与Build Number Maven Plugin集成
-				return;
-			}
-
-			if (cmd.hasOption("t")) {
-				// print the date and time
-				System.out.println(DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
-			} else {
-				// print the date
-				System.out.println(DateFormatUtils.format(new Date(), "yyyy-MM-dd"));
-			}
-
-		} catch (ParseException ex) {
-			// oops, something went wrong
-			System.err.println("Parsing failed.  Reason: " + ex.getMessage());
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
+			return cmd;
+		} catch (ParseException e) {
+			logger.fatal("parseCommandLine(): failed to parse options!", e);
+			System.exit(-1);
+			return null; // never return null
 		}
-
 	}
 
-	public static void main(String[] args) {
-		parseCommandLine(args);
+	public static void main(String[] args) throws Exception{
+		CommandLine cmd = parseCommandLine(args);
+		
+		if (0 == cmd.getOptions().length + cmd.getArgList().size() || cmd.hasOption("help")) {
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("help", options);
+			return;
+		}
+		
+		if (cmd.hasOption("server")) {
+			// io.netty.eventLoopThreads
+			String port = cmd.getOptionValue("server", "8000");
+			logger.warn("start agent server at port " + port);
+			new TimeServer(NumberUtils.toInt(port, 8000)).run();
+			return;
+		}
+
+		if (cmd.hasOption("version")) {
+			// 与Build Number Maven Plugin集成, 打包时把版本信息更新到assemble/build.properties
+			Properties buildInfo = new Properties();
+			try {
+				/**
+				 * search "add buildnumber to META-INF/MANIFEST.mf" in pom.xml
+				 * 
+				 */
+				InputStream is = App.class.getResourceAsStream("/assemble/build.properties");
+				buildInfo.load(is);
+				is.close();
+			} catch (Throwable t) {
+				System.err.println(t);
+			}
+			logger.info("Server Vendor:	" + buildInfo.getProperty("server.vendor","Unknow"));
+			logger.info("Server Version:	" + buildInfo.getProperty("server.version","Unknow"));
+			logger.info("SCM Tag:	" + buildInfo.getProperty("scm.tag","Unknow"));
+			logger.info("SCM Version:	" + buildInfo.getProperty("scm.version","Unknow"));
+			logger.info("Build Number:	" + buildInfo.getProperty("jenkins.number","Unknow"));
+			
+			return;
+		}
+
+		if (cmd.hasOption("t")) {
+			// print the date and time
+			logger.info(DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+		} else {
+			// print the date
+			logger.info(DateFormatUtils.format(new Date(), "yyyy-MM-dd"));
+		}
 	}
 }
